41735232155c5257e3406dea22d0149afb18b582
Carlos Salazar <carlosiusalazar@gmail.com>
2020-05-16 18:14:46 +0900

    Able to display liked movies as a table, needs to make the table to update after adding a new card to the right

--- src/components/FavList/index.jsx
+++ src/components/FavList/index.jsx
@@ -1,30 +1,10 @@
-import React from "react";
-import firebase from "firebase"
-import firebaseConfig from '../../fbInitialization'
-import { firestore } from "firebase";
-
-export default function FavList(props){
-
-  // const [favMovies, setFavMovies] = React.useState([])
-
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //   db = firebase.firestore();
-  //   const favMovieData = await db.collection("RealTable").get()
-  //   setFavMovies(favMovieData.docs.map(doc => doc.favMovieData()))
-  //   }
-  //   fetchData()
-  // },[])
-  console.log("favMovies state is ", props.favMovies)
-  return (
-    <ul>
-      {props.favMovies.map(movie => (
-        <li key={movie.Name}>{movie.Name}</li>
-      ))}
-    </ul>
-  );
-}
-
+// return (
+//   <ul>
+//     {props.favMovies.map(movie => (
+//       <li key={movie.Name}>{movie.Name + movie.Year}</li>
+//     ))}
+//   </ul>
+// );
 
 
 // return (
@@ -42,31 +22,70 @@ export default function FavList(props){
 //   );
 
 
+// {/* <table>
+//       <tbody>
+//         <tr>
+//         {db.collection("RealTable").orderBy('Name').onSnapshot((querySnapshot) => {
+//           querySnapshot.docs.forEach((doc) => {
+//               console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+//               return (
+//                 <>
+//                 <td>{doc.data().Name}</td>
+//                 <td>{doc.data().ImageLink}</td>
+//                 <td>{doc.data().Rating}</td>
+//                 <td>{doc.data().Year}</td>
+//                 <td>{doc.data().Plot}</td>
+//                 </>
+//             )
+//             })
+//           })}
+//         </tr>
+//     </tbody>
+//   </table> 
+//
 
 
 
+import React from "react";
+import firebase from "firebase"
+import firebaseConfig from '../../fbInitialization'
+import { firestore } from "firebase";
 
+export default function FavList(props){
+//   React.useEffect(() => {
+//     setTimeout(function(){ window.location.reload(true); }, 1);
+//  },[])
+  // const [favMovies, setFavMovies] = React.useState([])
 
-
-
-
-{/* <table>
+  // React.useEffect(() => {
+  //   const fetchData = async () => {
+  //   db = firebase.firestore();
+  //   const favMovieData = await db.collection("RealTable").get()
+  //   setFavMovies(favMovieData.docs.map(doc => doc.favMovieData()))
+  //   }
+  //   fetchData()
+  // },[])
+  console.log("favMovies state is ", props.favMovies)
+  return (
+    <table>
       <tbody>
-        <tr>
-        {db.collection("RealTable").orderBy('Name').onSnapshot((querySnapshot) => {
-          querySnapshot.docs.forEach((doc) => {
-              console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-              return (
-                <>
-                <td>{doc.data().Name}</td>
-                <td>{doc.data().ImageLink}</td>
-                <td>{doc.data().Rating}</td>
-                <td>{doc.data().Year}</td>
-                <td>{doc.data().Plot}</td>
-                </>
-            )
-            })
-          })}
-        </tr>
-    </tbody>
-  </table> */}
+        {
+             props.favMovies.map((movie,i) =>(
+                <tr key={i}>
+                 {
+                   //props.favMovies.map((mov,j)=>
+                      <div>
+                      <td key={i}>{movie.Name}</td>
+                      <td key={i}>{movie.Year}</td>
+                      <td key={i}>{movie.Rating}</td>
+                      {/* <td key={j}>{mov.Plot}</td> */}
+                      </div>
+                   //)
+                 }
+                </tr>
+             ))
+        }
+      </tbody>
+    </table>
+);
+};
