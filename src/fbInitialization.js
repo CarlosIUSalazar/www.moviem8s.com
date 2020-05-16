81c4657f272b625a8bdb6cb44b28209b4cb845e0
Saggitarie <saggitarie@gmail.com>
2020-05-16 11:33:13 +0900

    Remove unnecessary files and code

--- src/fbInitialization.js
+++ src/fbInitialization.js
@@ -1,34 +1,12 @@
-import firebase from 'firebase'
+const firebaseConfig = {
+  // Your web app's Firebase configuration
+    apiKey: "AIzaSyBLo8Qodatxni6vT2np4XGvNVgz1-XTHMY",
+    authDomain: "moviem8s.firebaseapp.com",
+    databaseURL: "https://moviem8s.firebaseio.com",
+    projectId: "moviem8s",
+    storageBucket: "moviem8s.appspot.com",
+    messagingSenderId: "223030327474",
+    appId: "1:223030327474:web:8f756c893c3cd008f6f1ab"
+  };
 
-// export default () => {
-  const firebaseConfig = {
-    // Your web app's Firebase configuration
-      apiKey: "AIzaSyBLo8Qodatxni6vT2np4XGvNVgz1-XTHMY",
-      authDomain: "moviem8s.firebaseapp.com",
-      databaseURL: "https://moviem8s.firebaseio.com",
-      projectId: "moviem8s",
-      storageBucket: "moviem8s.appspot.com",
-      messagingSenderId: "223030327474",
-      appId: "1:223030327474:web:8f756c893c3cd008f6f1ab"
-    };
-
-    export default firebaseConfig;
-    // firebase.initializeApp(firebaseConfig);
-    // const db = firebase.firestore();
-
-    // return db;
-// }
-
-// let firebaseConfig = {
-//     // Your web app's Firebase configuration
-//       apiKey: "AIzaSyBLo8Qodatxni6vT2np4XGvNVgz1-XTHMY",
-//       authDomain: "moviem8s.firebaseapp.com",
-//       databaseURL: "https://moviem8s.firebaseio.com",
-//       projectId: "moviem8s",
-//       storageBucket: "moviem8s.appspot.com",
-//       messagingSenderId: "223030327474",
-//       appId: "1:223030327474:web:8f756c893c3cd008f6f1ab"
-//     };
-
-//     let fire = firebase.initializeApp(firebaseConfig);
-//     export default fire;
+export default firebaseConfig;
