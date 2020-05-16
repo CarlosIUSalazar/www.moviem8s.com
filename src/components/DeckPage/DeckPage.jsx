572684c2099b3f88b17cf8885bb7db169a0e4879
Saggitarie <saggitarie@gmail.com>
2020-05-16 11:39:34 +0900

    Changed position for fav button

--- src/components/DeckPage/DeckPage.jsx
+++ src/components/DeckPage/DeckPage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Deck from "../Deck/Deck";
 import { Link } from "react-router-dom";
 
+import "./DeckPage.css";
+
 export default function DeckPage({db}){
   return (
     <div>
       {console.log("props passed to deckpage", db)}
       <Link to={"/favorite-movies"}>
-        <button>My Favorite Lists</button>
+        <button className="fav-button">My Favorite Lists</button>
       </Link>
       <Deck db={db}/>
     </div>
