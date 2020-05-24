a262bd1207213138cf5ece14392919b8109ba232
Carlos Salazar <carlosiusalazar@gmail.com>
2020-05-24 12:09:29 +0900

    changed section back to main in table for testing purposes

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -67,7 +67,7 @@ export default function Table(props){
             </div>
           </Link>
         </div>
-      <section style={{overflow : "scroll"}}>
+      <main style={{overflow : "scroll"}}>
         <div className="favTableContainer" >
           <div className="container-fluid" >
             <table className="table table-sm table-striped table-dark table-hover">
@@ -95,7 +95,7 @@ export default function Table(props){
         <div className="deleteAllMoviesButtonContainer">
               <button className="deleteAllMoviesButton" onClick={() => deleteMoviesOnTable()}>Delete All Movies</button>
         </div>
-      </section>
+      </main>
     </>
     )
 }
